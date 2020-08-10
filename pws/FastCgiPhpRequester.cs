35d41ad4a245d85479719909ec12532c98e7b41a
chaosannals <chaosannals@outlook.com>
2020-08-10 17:30:02 +0800

    分析

--- pws/FastCgiPhpRequester.cs
+++ pws/FastCgiPhpRequester.cs
@@ -19,24 +19,40 @@ namespace Pws
             Source = source;
         }
 
-        public IEnumerable<int> Transfer(TcpClient target)
+        public IEnumerable<FastCgiMessage> Transfer(TcpClient target)
         {
             byte[] buffer = new byte[8192];
             NetworkStream requester = Source.GetStream();
             NetworkStream responser = target.GetStream();
+            FastCgiParser sfcp = new FastCgiParser();
+            FastCgiParser tfcp = new FastCgiParser();
             while (true)
             {
                 if (requester.DataAvailable)
                 {
                     int count = requester.Read(buffer, 0, buffer.Length);
                     responser.Write(buffer, 0, count);
+                    sfcp.Gain(buffer, count);
+                    while (true)
+                    {
+                        FastCgiMessage m = sfcp.Pop();
+                        if (m == null) break;
+                        yield return m;
+                    }
                 }
                 if (responser.DataAvailable)
                 {
                     int count = responser.Read(buffer, 0, buffer.Length);
                     requester.Write(buffer, 0, count);
+                    tfcp.Gain(buffer, count);
+                    while (true)
+                    {
+                        FastCgiMessage m = tfcp.Pop();
+                        if (m == null) break;
+                        yield return m;
+                    }
                 }
-                yield return 0;
+                yield return null;
             }
         }
     }
